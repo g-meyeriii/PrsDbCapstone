461913db90609a5f3f0a0c295d0986bb2ded9e54
max-student <max-student@maxtrain.com>
2020-03-09 13:35:32 -0400

    Add project files.

---
+++ PrsDb/Data/PrsDbContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using PrsDb.Models;
+
+namespace PrsDb.Data
+{
+    public class PrsDbContext : DbContext
+    {
+        public PrsDbContext (DbContextOptions<PrsDbContext> options)
+            : base(options)
+        {
+        }
+        
+        public DbSet<User> Users { get; set; }
+        public DbSet<Vendor> Vendors { get; set; }
+
+        protected override void OnModelCreating (ModelBuilder model) {
+            model.Entity<User>(u => {
+                u.ToTable("Users");
+                u.HasKey(u => u.Id);
+                u.Property(u => u.UserName).HasMaxLength(30).IsRequired();
+                u.HasIndex(u => u.UserName).IsUnique();
+                u.Property(u => u.Password).HasMaxLength(30).IsRequired();
+                u.Property(u => u.FirstName).HasMaxLength(30).IsRequired();
+                u.Property(u => u.LastName).HasMaxLength(30).IsRequired();
+                u.Property(u => u.Phone).HasMaxLength(13);
+                u.Property(u => u.Email).HasMaxLength(255);
+                u.Property(u => u.IsReviewer).IsRequired().HasDefaultValue(false);
+                u.Property(u => u.IsAdmin).IsRequired().HasDefaultValue(false);
+            });
+            model.Entity<Vendor>(v => {
+                v.ToTable("Vendors");
+                v.HasKey(v => v.Id);
+                v.Property(v => v.Code).IsRequired().HasMaxLength(30);
+                v.HasIndex(v => v.Code).IsUnique();
+                v.Property(v => v.Name).HasMaxLength(30).IsRequired();
+                v.Property(v => v.Address).HasMaxLength(30).IsRequired();
+                v.Property(v => v.City).HasMaxLength(30).IsRequired();
+                v.Property(v => v.State).HasMaxLength(2).IsRequired();
+                v.Property(v => v.Zip).HasMaxLength(5).IsRequired();
+                v.Property(v => v.Phone).HasMaxLength(13);
+                v.Property(v => v.Email).HasMaxLength(255);
+            });
+            model.Entity<Product>(p => {
+                p.ToTable("Product");
+                p.HasKey(p => p.Id);
+                p.Property(p => p.PartNbr).HasMaxLength(30).IsRequired();
+                p.Property(p => p.Name).HasMaxLength(30).IsRequired();
+                p.Property(p => p.Price).HasMaxLength(13);
+                p.Property(p => p.Unit).HasMaxLength(30).IsRequired();
+                p.Property(p => p.PhotoPath).HasMaxLength(255);
+                          
+            });
+            model.Entity<RequestLine>(rl => {
+                rl.ToTable("RequestLine");
+                rl.HasKey(rl => rl.Id);
+                rl.Property(rl => rl.Quantity).HasDefaultValue(1);
+            });
+        }
+
+
+        
+    }
+}
---
+++ PrsDb/Models/RequestLine.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PrsDb.Models {
+    public class RequestLine {
+        public int Id { get; set; }
+        public int RequestId { get; set; }
+        public int ProductId { get; set; }
+        public int Quantity { get; set; }
+
+        
+        public virtual Request Request { get; set; }
+        public virtual Product Product { get; set; }
+        RequestLine() { }
+
+    }
+}
