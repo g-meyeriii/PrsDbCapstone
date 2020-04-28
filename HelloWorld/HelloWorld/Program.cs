f82294d906b40e7a586037ddda63168338f6d90b
gjmiii9@gmail.com <gjmiii9@gmail.com>
2020-04-28 14:37:17 -0700

    bool, float, double, decimal, cast

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -6,9 +6,47 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello George");
+            //string myname = "George";
+
+            //string message = "My name is " + myname;
+
+            //string capsmessage = message.ToUpper();
+
+            //string lowermessage = message.ToLower();
+
+
+            //Console.WriteLine(lowermessage);
+
+            //Console.Read();
+
+            // implicit conversion
+            //int num = 1290532;
+            //long bignum = num;
+
+            //Console.WriteLine(bignum);
+            //Console.Read();
+
+            float myFloat = 13.37f;
+            double myNewDouble = myFloat;
+
+            double myDouble = 13.37;
+            int myInt;
+           
+            // explicit conversion
+            // cast double to int;
+            myInt = (int)myDouble;
+
+            // typeconversion
+            string myString = myDouble.ToString(); // "13.37"
+            string myFloatString = myFloat.ToString();
+
+            bool sunIsShining = true;
+
+            string myBoolString =sunIsShining.ToString();
+
+            Console.WriteLine(myBoolString);
             Console.Read();
         }
+
     }
 }
